230f3f0a51958e239cc1e925039935d44e66c593
EricLi3 <ericlibeibei@gmail.com>
2024-05-31 15:26:37 -0400

    with landing page done, features for viewing trailers and adding comments which are stored in mongoDB database.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
+
 
 function App() {
 
   //useState returns array with current state element and Function to update it as an array.
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
 
   // get all movies
   const getMovies = async () => {
@@ -24,14 +35,43 @@ function App() {
     }
 
   }
+
+  const getMovieData = async (movieId) => {
+    try
+    {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      
+      const singleMovie = response.data;
+      
+      console.log(response.data);
+      
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+    }
+    catch(error)
+    {
+      console.error(error);
+    }
+  }
+
   // so getMovies compoent is called when the component is loaded
   useEffect(() => {
     getMovies();
   }, []);
 
+  // Add Components here. 
   return (
     <div className="App">
-     
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path ="/Reviews/:movieId" element={<Reviews getMovieData = {getMovieData} movie = {movie} reviews = {reviews} setReviews = {setReviews}  />}></Route>
+          <Route path="*" element = {<NotFound/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://f10a-47-14-142-232.ngrok-free.app',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL: 'https://movie-social-chat-backend.onrender.com/'           //'https://f10a-47-14-142-232.ngrok-free.app',
+    //headers: {"ngrok-skip-browser-warning": "true"}
 });
